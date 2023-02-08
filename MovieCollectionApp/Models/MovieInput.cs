6f5e1b93d5a63ef6f087f6ada9da4d33e4de01a4
Tanner Funk <tannerfunk4@gmail.com>
2023-02-07 18:43:47 -0700

    Add project files.

---
+++ MovieCollectionApp/Models/MovieInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieCollectionApp.Models
+{
+    public class MovieInput
+    {
+        public string Category { get; set; }
+
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public string Director { get; set; }
+
+        public string Rating { get; set; }
+
+        public bool Edited { get; set; }
+
+        public string LentTo { get; set; }
+
+        public string Note { get; set; }
+
+    }
+}
